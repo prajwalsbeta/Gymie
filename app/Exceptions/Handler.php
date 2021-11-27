5435491c0711be0628460093360423510de01e02
prajwalsbeta <thelolthelolthelol19@gmail.com>
2021-11-28 01:00:26 +0530

    Improved comments

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -10,7 +10,7 @@ class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that should not be reported.
-     *
+     * Block list for errors
      * @var array
      */
     protected $dontReport = [
